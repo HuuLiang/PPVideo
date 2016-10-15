218dc34633c7a000c9542efea183ed75213b3f3a
Liang <q93867@yeah.net>
2016-10-15 10:17:10 +0800

    create project

---
+++ PPVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PPVideo
+//
+//  Created by Liang on 2016/10/15.
+//  Copyright © 2016年 Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PPVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PPVideo
+//
+//  Created by Liang on 2016/10/15.
+//  Copyright © 2016年 Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
