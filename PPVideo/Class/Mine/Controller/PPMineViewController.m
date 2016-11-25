a58c43b7fad00f8a3a4ccd64765728045eb08941
Liang <q93867@yeah.net>
2016-11-25 10:52:33 +0800

    delete 4

--- PPVideo/Class/Mine/Controller/PPMineViewController.m
+++ PPVideo/Class/Mine/Controller/PPMineViewController.m
@@ -214,7 +214,7 @@ QBDefineLazyPropertyInitialization(PPAppModel, appModel)
 - (void)initQQCellInSection:(NSInteger)section {
     [self setHeaderHeight:kWidth(20) inSection:section];
     
-    _qqCell = [[PPTableViewCell alloc] initWithImage:[UIImage imageNamed:@"mine_qq"] title:@"在4线客服"];
+    _qqCell = [[PPTableViewCell alloc] initWithImage:[UIImage imageNamed:@"mine_qq"] title:@"在线客服"];
     _qqCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     _qqCell.selectionStyle = UITableViewCellSelectionStyleNone;
     [self setLayoutCell:_qqCell cellHeight:tableViewCellheight inRow:0 andSection:section];
